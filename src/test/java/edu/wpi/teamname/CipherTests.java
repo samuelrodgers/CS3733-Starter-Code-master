c5f4f76939a656a61f9914f5718660433c5b88bf
samuelrodgers <srodgers@wpi.edi>
2020-05-08 16:45:58 -0400

    ceasar tests

--- src/test/java/edu/wpi/teamname/CipherTests.java
+++ src/test/java/edu/wpi/teamname/CipherTests.java
@@ -1,6 +1,7 @@
 package edu.wpi.teamname;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 import edu.wpi.teamname.entity.CaesarCipher;
 import edu.wpi.teamname.entity.ElbonianCipher;
@@ -13,19 +14,41 @@ import org.junit.Test;
 public class CipherTests {
 
   @Test
-  public void cipherSimpleTest1() {}
-
-  @Test
-  public void ceasarTest() {
-    CaesarCipher ceasar = new CaesarCipher();
-    ceasar.setText("");
-    assertEquals("", ceasar.getText());
+  public void cipherSimpleTest1() {
+    assertTrue(ceasarTest1());
+    assertTrue(ceasarTest2());
+    assertTrue(ceasarTest3());
+    assertTrue(elbonianTest1());
+    assertTrue(elbonianTest2());
   }
 
-  @Test
-  public void elbonianTest() {
+  public boolean ceasarTest1() {
+    CaesarCipher ceasar = new CaesarCipher();
+    ceasar.setText("This is a boring message!");
+    return ceasar.getText().equals("Gsrh rh z ylirmt nvhhztv!");
+  }
+
+  public boolean ceasarTest2() {
+    CaesarCipher ceasar = new CaesarCipher();
+    ceasar.setText("Don't fall over the railing");
+    return ceasar.getText().equals("Kvu'a mhss vcly aol yhpspun");
+  }
+
+  public boolean ceasarTest3() {
+    CaesarCipher ceasar = new CaesarCipher();
+    ceasar.setText("S&P500");
+    return ceasar.getText().equals("Z&W500");
+  }
+
+  public boolean elbonianTest1() {
     ElbonianCipher elbonian = new ElbonianCipher();
     elbonian.setText("");
-    assertEquals("", elbonian.getText());
+    return elbonian.getText().equals("");
+  }
+
+  public boolean elbonianTest2() {
+    ElbonianCipher elbonian = new ElbonianCipher();
+    elbonian.setText("");
+    return elbonian.getText().equals("");
   }
 }
